f80907963c8e1b4f4708fd57f54d8e55bf21f5ce
Erik <cs150-br@imail.eecs.berkeley.edu>
2012-10-16 04:04:03 -0700

    Rough top-level sketch

--- hardware/src/MIPS150.v
+++ hardware/src/MIPS150.v
@@ -5,10 +5,26 @@ module MIPS150(
     output FPGA_SERIAL_TX
 );
 
-// Use this as the top-level module for your CPU. You
-// will likely want to break control and datapath out
-// into separate modules that you instantiate here.
 
+StageWF wf
+	(.clk(clk), .rst(rst), .stall(stall),
+	.PCWF_(PCWF_), .PCNext_DX_(PCNext_DX_),
+	.INST(INST)
+	);
+StageDX dx
+	(.clk(clk), .rst(rst), .stall(stall),
+	.PC_WF(PCWF_), .PCNext_DX_(PCNext_DX_)
+	);
+StageM  m
+	(.clk(clk), .rst(rst), .stall(stall)
+	);
+
+iblk iblk
+	(xyz);
+dblk dblk
+	(pdq);
+uart uart
+	(abc);
 
 
 endmodule
