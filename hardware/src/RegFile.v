783268a6c7a5f4071489b8e1da88fafcb5d93390
Erik <cs150-br@imail.eecs.berkeley.edu>
2012-10-16 03:04:59 -0700

    Simple RegFile

--- hardware/src/RegFile.v
+++ hardware/src/RegFile.v
@@ -14,12 +14,26 @@
 //  Author: <<YOUR NAME HERE>>
 //-----------------------------------------------------------------------------
 
-module RegFile(input clk,
-               input we,
-               input  [4:0] ra1, ra2, wa,
+module RegFile(input         clk,
+               input         we,
+               input  [4:0]  ra1,
+               input  [4:0]  ra2,
+               input  [4:0]  wa,
                input  [31:0] wd,
-               output [31:0] rd1, rd2);
+               output [31:0] rd1,
+               output [31:0] rd2);
 
-// Implement your register file here, then delete this comment.
+// The dist-ram is already "true dual port", using coordinated writes
+//   to two banks and separate asynchronous reads.  Otherwise, we could
+//   mimic this ourselves with duplicate register banks.
 
+    (* ram_style = "distributed" *) reg [31:0] R [31:1];
+	always @(posedge clk) begin
+		if (we && (wa != 5'd0)) begin
+			R[wa] <= wd;
+		end
+	end
+
+	assign rd1 = (ra1 == 5'd0) ? 32'd0 : R[ra1];
+	assign rd2 = (ra2 == 5'd0) ? 32'd0 : R[ra2];
 endmodule
