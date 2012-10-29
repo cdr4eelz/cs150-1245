f9b4605ad43456caf4507b6c7bf96ef980715e02
Erik <ejrogers@berkeley.edu>
2012-10-29 02:13:00 -0700

    Changed log output of rsim.sh

--- hardware/sim/rsim.sh
+++ hardware/sim/rsim.sh
@@ -12,7 +12,7 @@ $RSYNC --exclude [id]mem_blk_ram ~/$BASEDIR/src/ $REMOTER:~/$BASEDIR/src
 $RSYNC ~/$BASEDIR/sim/tests/ $REMOTER:~/$BASEDIR/sim/tests
 
 
-ssh $REMOTER "cd ~/$BASEDIR/sim ; make $TARG | tee rtrans.txt"
+ssh $REMOTER "cd ~/$BASEDIR/sim ; make $TARG | tee results/rsim.log"
 
 $RSYNC $REMOTER:~/$BASEDIR/sim/results/ ~/$BASEDIR/sim/results
 
