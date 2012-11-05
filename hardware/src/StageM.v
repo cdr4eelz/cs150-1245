2d588e09aaa24baa09b25861107a653c3c49de55
Erik <ejrogers@berkeley.edu>
2012-11-05 14:45:11 -0800

    Beginning StageM testing...might do vector style or mini-CPU

--- hardware/src/StageM.v
+++ hardware/src/StageM.v
@@ -3,13 +3,13 @@
 `include "CPUBusses.vh"
 
 module StageM(
-    inout `BUS_CPUGlobal_type   CPUGlobal,  // Not used (at least not clock, yet)
+    inout `BUS_CPUGlobal_type   CPUGlobal,  // Clock not used (others not used yet either)
     inout `BUS_MEMIO_type       MemoryIO,
     
     input `BUS_ICTL_type        IControl,
     input  [31: 0]  ALUOut,
-    input  [31: 0]  R2Value,
-    input  [31: 0]  PCPLUS8,
+    input  [31: 0]  R2Value,    //TODO: Beter name?!?
+    input  [31: 0]  PCPLUS8,    //TODO: Consider changing to PC (if no pre-compute argument holds up)
     
     output [ 4: 0]  WBK_Reg_,
     output [31: 0]  WBK_Val_
@@ -17,18 +17,19 @@ module StageM(
     parameter crap = "tastic";
     wire   [ 3: 0] WriteByteMask = (`ICTL_MemWrite(IControl)) ? 4'b1111 : 4'b0000;
                     //TODO: ^^^ Temporarily write to all bytes ^^^
-    wire   [31: 0] DataWrite = R2Value;
+    wire   [11: 0] AddressRW = ALUOut; // TODO: Drive with `Unknown when ~|WriteByteMask
+    wire   [31: 0] DataWrite = R2Value;// ^^^this too.
     wire   [31: 0] DataRead;
     
     BUS_MEMIO_tun
     ( ._BUS_(MemoryIO),
-        .Addr(ALUOut), //TODO: Drive with Unknown when not MemWrite
+        .Addr(AddressRW),
         .WEnab(WriteByteMask),
         .WData(DataWrite),
         .RData(DataRead)
     );
     
-    assign WBK_Reg_ = `ICTL_DestReg(IControl);
+    assign WBK_Reg_ = `ICTL_DestReg(IControl); // Expected to be zero when no writeback
     assign WBK_Val_ = `ICTL_MemToReg(IControl) ? DataRead : 
                         `ICTL_Link(IControl) ? PCPLUS8 : ALUOut;
     
---
+++ hardware/src/testbench/StageMTestbench.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+
+`include "Opcode.vh"
+`include "CPUBusses.vh"
+
+module StageMTestbench;
+
+    // No Clock Signal, just used to punctuate test sampling time
+    reg Clock;
+    
+    `BUS_CPUGlobal_type CPUGlobal;
+    `BUS_MEMIO_type     MemoryIO;
+    `BUS_ICTL_type      IControl;
+    
+    reg  [31:0] PC, ALUOut, R2Value;
+    wire [ 5:0] WBK_Reg;
+    wire [31:0] WBK_Val;
+    
+    StageM(
+        .CPUGlobal(CPUGlobal), .MemoryIO(MemoryIO),
+        .IControl(IControl), .ALUOut(ALUOut), .R2Value(R2Value), .PCPLUS8(PC+8),
+        .WBK_Reg_(WBK_Reg), .WBK_Val_(WBK_Val)
+    );
+    
+    integer step = 0;
+    
+    task exec_inst;
+        input [31:0] inst;
+        reg [31:0] pc;
+        begin
+            step = step + 1;
+        end
+    endtask
+    
+    initial begin
+        $display("All tests passed!");
+        $finish();
+    end
+endmodule
