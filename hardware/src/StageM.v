3ccb1c9250e026c3a2f4ad95c700de280537dd5d
Erik <ejrogers@berkeley.edu>
2012-11-12 00:53:03 -0800

    Reverted to ALUOut as generic name since the value is needed pre & post pipeline register and has dual meaning as either memory address or value to write-back to a register.

--- hardware/src/StageM.v
+++ hardware/src/StageM.v
@@ -8,9 +8,9 @@ module StageM(
     
     input `BUS_ICTL_type        _IControl,
     input `BUS_ICTL_type        IControl,
-    input  [31: 0]  _MemAddr,
+    input  [31: 0]  _ALUOut,
     input  [31: 0]  _MemWValue,
-    input  [31: 0]  RegWValue,
+    input  [31: 0]  ALUOut,
     input  [31: 0]  PCPLUS8,
     
     output [ 4: 0]  WBK_Reg_,
@@ -18,21 +18,21 @@ module StageM(
     output          WBK_CanFWD_
 );
     parameter crap = "tastic";
-    wire  [ 3: 0] _TargetMask  = _MemAddr[31:28];  // Top nibble to direct target
-    wire  [11: 0] _AddressRW   = _MemAddr[13:2];   // TODO: Drive with `Unknown when ~|WriteByteMask
-    wire  [ 1: 0] _SubAddr     = _MemAddr[1:0];
-    wire  [ 1: 0] _SubWidth    = (3 - `ICTL_DataWidth(_IControl));
-    wire  [ 3: 0] _WByteMask   = (!`ICTL_MemWrite(_IControl)) ? 4'b0000 :
+    wire  [ 3: 0] _TargetMask   = _ALUOut[31:28];  // Top nibble to direct target
+    wire  [11: 0] _Address      = _ALUOut[13:2];   // TODO: Drive with `Unknown when ~|WriteByteMask
+    wire  [ 1: 0] _SubAddr      = _ALUOut[1:0];
+    wire  [ 1: 0] _SubWidth     = (3 - `ICTL_DataWidth(_IControl));
+    wire  [ 3: 0] _WByteMask    = (!`ICTL_MemWrite(_IControl)) ? 4'b0000 :
                                     4'b1111 << _SubWidth >> _SubAddr;
-    wire  [31: 0] _WDataMasked = _MemWValue << (_SubWidth*8) >> (_SubAddr*8);
+    wire  [31: 0] _WDataMasked  = _MemWValue << (_SubWidth*8) >> (_SubAddr*8);
     
     wire  [31: 0] DataRead;
     BUS_MEMIO_tun BUS_MEMIO
     ( ._BUS_(MemoryIO),
-        .Addr   (_AddressRW),   // TODO: Drive with `Unknown when not reading or writing
+        .Addr   (_Address),     // TODO: Drive with `Unknown when not reading or writing
         .TMask  (_TargetMask),
         .BMask  (_WByteMask),
-        .WData  (_WDataMasked),   // ^^^these too.
+        .WData  (_WDataMasked), // ^^^these too.
         .RData  (DataRead)      // This is registered by the memory itself
     );
     
@@ -40,15 +40,16 @@ module StageM(
     //   control signals, ets. (Example, DataWidth for read comes from IControl but
     //   DataWidth for write came from _IControl).
     
-    wire  [ 1: 0] SubAddr   = RegWValue[1:0];
+    wire  [ 1: 0] SubAddr   = ALUOut[1:0];
     wire  [ 1: 0] SubWidth  = (3 - `ICTL_DataWidth(IControl));
     wire  [31: 0] DataLoad  = DataRead << (SubAddr*8) >> (SubWidth*8);
     // ^^ I guess I'm hoping this stuff washes out with optimization! Is nice to have so
     //    expanded out like this for debugging/simulation purposes, though :)
-
+    
+    // Might divorce WBK from FWD stuff more fully to clarify slightly different paths
     assign WBK_Reg_ = `ICTL_DestReg(IControl); // Expected to be zero when no writeback
     assign WBK_Val_ = `ICTL_MemToReg(IControl) ? DataLoad :
-                        `ICTL_Link(IControl) ? (PCPLUS8) : RegWValue;
+                        `ICTL_Link(IControl) ? (PCPLUS8) : ALUOut;
     assign WBK_CanFWD_ = !`ICTL_MemToReg(IControl) && (WBK_Reg_ != 0);
     
 endmodule
