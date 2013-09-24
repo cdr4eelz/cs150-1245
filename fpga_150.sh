11f247d404f7d5ccaad2669f53e204f9a40abb60
Erik Rogers <ejrogers@berkeley.edu>
2013-09-23 23:51:42 -0700

    FPGA script for laptop/home.

---
+++ fpga_150.sh
@@ -0,0 +1,9 @@
+#source /opt/Xilinx/14.1/ISE_DS/settings64.sh /opt/Xilinx/14.1/ISE_DS
+source /opt/Xilinx/default/ISE_DS/settings64.sh
+export PATH=/opt/modeltech/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/lib64:${LD_LIBRARY_PATH}
+
+export MGLS_LICENSE_FILE=1717@license-srv.eecs.berkeley.edu:1717@sunv40z-1.eecs.berkeley.edu:1717@sunv20z-1.eecs.berkeley.edu
+export XILINXD_LICENSE_FILE=/opt/Xilinx.lic:2100@license-srv.eecs.berkeley.edu
+
+alias fpga_editor='DISPLAY=:0 fpga_editor'
