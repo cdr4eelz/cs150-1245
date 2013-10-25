1bf11872290efbc20cefcfc7928096d0b0a3fb25
Erik Rogers <ejrogers@berkeley.edu>
2013-10-25 07:49:33 -0700

    Prep plop for circular buffers (pending).

--- tool/plop/plop/src/plop.c
+++ tool/plop/plop/src/plop.c
@@ -17,46 +17,42 @@
 
 inline u8 pumpBytes_raw(int u_from, int u_to, u8 special)
 {
-	//u8 count = 0;
-	while(!XUartLite_IsReceiveEmpty(u_from) && !XUartLite_IsTransmitFull(u_to)) {
-		u8 bX = XUartLite_RecvByte(u_from);
-		if (special && (bX=='\n')) {
-			XUartLite_SendByte(u_to, '\r');
-		} else XUartLite_SendByte(u_to, bX);
-		//count++;
-		return 1;
-	}
-	//return count;
-	return 0;
+    //u8 count = 0;
+    while(!XUartLite_IsReceiveEmpty(u_from) && !XUartLite_IsTransmitFull(u_to)) {
+        u8 bX = XUartLite_RecvByte(u_from);
+        if (special && (bX=='\n')) bX = '\r';
+        XUartLite_SendByte(u_to, bX);
+        //count++;
+        return 1;
+    }
+    //return count;
+    return 0;
 }
 
-u8 tX[256];
-u8 rX[256];
+#define bufSize 2048
+u8 tX[bufSize];
+u8 rX[bufSize];
 
 int main()
 {
     init_platform();
 
-    print("\r\n[RELAY.x]\n\r");
-	while (1) {
+    print("\r\n[RELAY.b]\n\r");
+
+    u8 *pTH = tX, *pTT = tX;
+    u8 *pRH = rX, *pRT = rX;
+    u8 bX;
+    while (1) {
         while (!XUartLite_IsReceiveEmpty(U_DEBUG) && !XUartLite_IsTransmitFull(XPAR_RS232_UART_2_BASEADDR)) {
-            tX[0] = XUartLite_RecvByte(U_DEBUG);
-            if (tX[0] == '\n') tX[0] = '\r';
-            if (tX[0] != 0) {
-                //if (!XUartLite_IsTransmitFull(XPAR_RS232_UART_2_BASEADDR)) {
-                    XUartLite_SendByte(XPAR_RS232_UART_2_BASEADDR, tX[0]);
-                //}
-            }
+            bX = XUartLite_RecvByte(U_DEBUG);
+            if (/*special &&*/ (bX=='\n')) bX = '\r';
+            if (bX != 0) XUartLite_SendByte(XPAR_RS232_UART_2_BASEADDR, bX);
         }
-        if (!XUartLite_IsReceiveEmpty(XPAR_RS232_UART_2_BASEADDR) && !XUartLite_IsTransmitFull(U_DEBUG)) {
-            rX[0] = XUartLite_RecvByte(XPAR_RS232_UART_2_BASEADDR);
-            if (rX[0] != 0) {
-                //if (!XUartLite_IsTransmitFull(U_DEBUG)) {
-                    XUartLite_SendByte(U_DEBUG, rX[0]);
-                //}
-            }
+        while (!XUartLite_IsReceiveEmpty(XPAR_RS232_UART_2_BASEADDR) && !XUartLite_IsTransmitFull(U_DEBUG)) {
+            bX = XUartLite_RecvByte(XPAR_RS232_UART_2_BASEADDR);
+            if (bX != 0) XUartLite_SendByte(U_DEBUG, bX);
         }
-	}
+    }
     print("\r\n[DONE]\r\n");
 
     cleanup_platform();
