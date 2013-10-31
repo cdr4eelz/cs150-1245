a9efe79ad483aeadc411a82c7e3aaa29b6a45f5f
Erik Rogers <ejrogers@berkeley.edu>
2013-10-30 18:10:58 -0700

    Software/bram build adjustments to help keep memory addresses clearer (linking, pointers, hw-boot, coe-injections, etc.).

---
+++ software/as_scratch.s
@@ -0,0 +1,26 @@
+.section    .start
+.global     _start
+
+_start:
+    lui     $v0, 0
+    lui     $v1, 0
+    lui     $a0, 0
+    lui     $a1, 0
+    lui     $a2, 0
+    lui     $a3, 0
+    lui     $s0, 0
+    lui     $s1, 0
+    lui     $s2, 0
+    lui     $s3, 0
+    lui     $s4, 0
+    lui     $s5, 0
+    lui     $s6, 0
+    lui     $s7, 0
+    lui     $k0, 0
+    lui     $k1, 0
+    li 	    $gp, 0x50003000
+    li      $fp, 0x50004000
+    li      $sp, 0x50004000
+    li      $ra, 0x60000000
+    j       main
+    nop
