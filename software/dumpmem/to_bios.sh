71719945f851fddda23466b87fb69c0e7a0cd539
Erik Rogers <ejrogers@berkeley.edu>
2013-11-18 20:23:36 -0800

    linebreaks & dumpmem warning

---
+++ software/dumpmem/to_bios.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp dumpmem.inst.coe ~/team45/hardware/src/bios_mem/bios_mem.coe
+cp dumpmem.inst.coe ~/winhome/simmy/bios_mem.mif
+
+cp dumpmem.data.coe ~/team45/hardware/src/dmem_blk_ram/dmem_blk_ram.coe
+cp dumpmem.data.coe ~/winhome/simmy/dmem_blk_ram.mif
