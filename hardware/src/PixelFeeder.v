18dd0d4b54259572d4b5b6407af54d181249e60b
Erik Rogers <ejrogers@berkeley.edu>
2014-01-21 00:41:27 -0800

    Some more PixelFeeder (though set to do test pattern through fifo).

--- hardware/src/PixelFeeder.v
+++ hardware/src/PixelFeeder.v
@@ -2,7 +2,7 @@
 //TODO: Make test patterns selectable via software (and keypress)
 
 module PixelFeeder #(
-    parameter COLT45_TESTPAT=0
+    parameter COLT45_TESTPAT=1
 ) (                 //System:
                     input          cpu_clk_g,
                     input          clk50_g, // DVI Clock
@@ -21,23 +21,16 @@ module PixelFeeder #(
 
 		    output frame_interrupt);
 
-    wire feeder_den, feeder_empty, feeder_full;
-    wire [127:0] feeder_din;
-
     // Hint: States
     localparam IDLE = 1'b0;
     localparam FETCH = 1'b1;
 
     localparam PIXFO_CAPACITY = 256; //Max # 128-bit writes to fill
-    localparam PIXFO_TARGET = PIXFO_CAPACITY - 2; //Each read request yields 2
+    localparam PIXFO_TARGET = PIXFO_CAPACITY - 7; //1 af req => 2 x 128-bit rdf => 8 x 32-bit pixfo rd
 
     localparam FRAME0_SEL = 2'b01, FRAME1_SEL = 2'b10;
     //localparam FRAME0_BASE = 32'h1040_0000, FRAME1_BASE = 32'h1080_0000;
     //address = {8'h10, framesel, y, x, 2’b0}  _01 or _10 framesel are high bits of 4 or 8 in base.
-
-
-generate if (COLT45_TESTPAT == 0) begin:PIXFO_DDREAD
-    assign feeder_den = rdf_valid, feeder_din = rdf_dout; //DDR-read to PIX-write
 
     // 1-request => 2-responses => 8-pixels (all 256-bits).
     // Don't care about individual pixels in PixelFeeder!
@@ -53,58 +46,82 @@ generate if (COLT45_TESTPAT == 0) begin:PIXFO_DDREAD
     //Traverse row & col, flip source frame & fire interrupt (pulse) between.
     //Once arbiter gives attention, a read will ensure (two chunks). (mimic "Cache.v")
 
-// Cross-clock signals
-    reg chunk_ack; //From DVI-clock realm
-    reg chunk_inc; //From CPU-clock realm
+// Cross-clock signal & acknowledge (using 4-cycle ack technique from Fall-13)
+    reg  chunk_inc; //From DVI-clock realm
+    reg  chunk_ack; //From CPU-clock realm
+    //NOTE: Cross-clock async registers might need ASYNC_REG=TRUE
 
-// CPU-Clocked region
+// DVI-Clocked region (clk50_g)
+
+    /* We drop the first frame to allow the buffer to fill with data from
+    * DDR2. This gives alignment of the frame. */
+    reg  [31:0] ignore_count;
+    reg  [ 3:0] count_dviread; //Rolls over for each 16 pixel "read-chunk"
+    reg  rst_clk50, running_reg, chunk_ack_clk50;
+
+    always @(posedge clk50_g) begin
+        rst_clk50 <= rst; //Synchronize to DVI-clock
+        if (rst_clk50) begin //Use synchronized reset
+            ignore_count <= 32'd480000; // 600*800 
+            {running_reg, chunk_ack_clk50, count_dviread, chunk_inc} <= 0;
+        end else begin
+            running_reg <= 1'b1;
+            chunk_ack_clk50 <= chunk_ack; //Synchronize to DVI-clock
+            if (video_ready && running_reg && (ignore_count != 0)) begin
+                ignore_count <= ignore_count - 32'b1;
+            end if (video_ready && running_reg) begin
+                count_dviread <= count_dviread+1;
+                if (&count_dviread) chunk_inc <= 1'b1; //Set on rollover
+                else if (chunk_ack_clk50) chunk_inc <= 1'b0;
+            end else if (chunk_ack_clk50) chunk_inc <= 1'b0;
+        end
+    end
+
+    // FIFO to buffer the reads with a write width of 128 and read width of 32. We try to fetch blocks
+    // until the FIFO is full.
+    wire [31:0] feeder_dout;
+    wire feeder_den, feeder_full, feeder_empty;
+    wire [127:0] feeder_din;
+
+    pixel_fifo feeder_fifo(
+    	.rst(rst), //Depending on coregen settings, reset is synchronized internally
+    	.wr_clk(cpu_clk_g),
+    	.wr_en(feeder_den), //rdf_valid
+    	.din(feeder_din), //rdf_dout
+    	.full(feeder_full),
+    	.rd_clk(clk50_g),
+    	.rd_en(video_ready && running_reg && (ignore_count == 0)),
+    	.dout(feeder_dout),
+    	.empty(feeder_empty));
+
+
+generate if (COLT45_TESTPAT == 0) begin:PIXFO_DDREAD
+// *** Normal PixelFeeder activity (DDR -> FIFO) ***
+
+// CPU-Clocked region (cpu_clk_g)
 
     wire [30:0] next_addr;
-    wire [9:0] head_y = 10'd0, head_x = 10'd0;
+    wire [ 9:0] head_y = 10'd0, head_x = 10'd0;
     assign next_addr = {7'b000_0000, FRAME0_SEL, head_y, head_x, 2'b00};
-
+    reg chunk_inc_clkCPU;
 
     always @(posedge cpu_clk_g) begin
         if (rst) begin
-            chunk_inc_clkcpu <= 0;
+            chunk_inc_clkCPU <= 0;
             chunk_ack <= 0;
         end else begin
-            chunk_inc_clkcpu <= chunk_inc;
-            chunk_ack <= chunk_inc_clkcpu;
+            chunk_inc_clkCPU <= chunk_inc;
+            chunk_ack <= chunk_inc_clkCPU;
         end
     end
 
-// DVI-Clocked region
-
-    reg [3:0] count_dviread; //Rolls over for each 16 pixel "read-chunk"
-
-    reg rst_clk50, chunk_ack_clk50;
-    always @(posedge clk50_g) begin
-        rst_clk50 <= rst;
-        chunk_ack_clk50 <= chunk_ack;
-        if (rst_clk50) begin
-            count_dviread <= 0;
-            chunk_inc <= 0;
-        end else if (video_ready) begin
-            count_dviread <= count_dviread+1;
-            if (&count_dviread) chunk_inc <= 1'b1;
-            else if (chunk_ack_clk50) chunk_inc <= 1'b0;
-        end
-    end
-
-
-
-
-
-
-
-
-
-
-
+    assign feeder_den = rdf_valid, feeder_din = rdf_dout; //DDR-read to PIX-write
+    assign video_valid = running_reg, video = feeder_dout[23:0];
 
 
 end else if (COLT45_TESTPAT == 1) begin:PIXFO_SWEEP
+// *** Simple test pattern output through the FIFO ***
+
     reg [15:0] sweep_RGB;
     reg [31:0] sweep_cnt;
     always @(posedge cpu_clk_g) begin
@@ -116,6 +133,7 @@ end else if (COLT45_TESTPAT == 1) begin:PIXFO_SWEEP
             sweep_cnt <= sweep_cnt+1; //Sent another 4 pixels
         end
     end
+
     assign feeder_den = !feeder_full;
     assign feeder_din = {
         8'd0, 24'h808080, // Grey stripe
@@ -123,43 +141,11 @@ end else if (COLT45_TESTPAT == 1) begin:PIXFO_SWEEP
         8'd0, sweep_RGB[15:8], sweep_RGB[11:4], sweep_RGB[7:0],
         8'd0, sweep_RGB[15:8], sweep_RGB[11:4], sweep_RGB[7:0]
     };
-end endgenerate
+    assign video_valid = running_reg, video = feeder_dout[23:0];
 
-
-generate if (COLT45_TESTPAT <= 1) begin:PIXEL_FIFO
-    /* We drop the first frame to allow the buffer to fill with data from
-    * DDR2. This gives alignment of the frame. */
-    reg  [31:0] ignore_count;
-
-    always @(posedge clk50_g) begin //NOTE: Was cpu_clk_g in skeleton
-       if(rst)
-            ignore_count <= 32'd480000; // 600*800 
-       else if(ignore_count != 0 & video_ready)
-            ignore_count <= ignore_count - 32'b1;
-       else
-            ignore_count <= ignore_count;
-    end
-
-    // FIFO to buffer the reads with a write width of 128 and read width of 32. We try to fetch blocks
-    // until the FIFO is full.
-    wire [31:0] feeder_dout;
-
-    pixel_fifo feeder_fifo(
-    	.rst(rst),
-    	.wr_clk(cpu_clk_g),
-    	.rd_clk(clk50_g),
-    	.din(feeder_din), //rdf_dout
-    	.wr_en(feeder_den), //rdf_valid
-    	.rd_en(video_ready & (ignore_count == 0)),
-    	.dout(feeder_dout),
-    	.full(feeder_full),
-    	.empty(feeder_empty));
-
-    assign video = feeder_dout[23:0];
-    assign video_valid = 1'b1;
 
 end else if (COLT45_TESTPAT == 2) begin:DIRECT_SWEEP
-    // DIRECTLY send a pretty changing pattern
+// *** DIRECTLY send a pretty and scrolling pattern ***
     reg [15:0] sweep_RGB;
     assign video = {sweep_RGB[15:8], sweep_RGB[11:4], sweep_RGB[7:0]};
     assign video_valid = 1'b1;
@@ -168,8 +154,9 @@ end else if (COLT45_TESTPAT == 2) begin:DIRECT_SWEEP
         else if (video_valid && video_ready) sweep_RGB <= sweep_RGB+5;
     end
 
-end else begin:DIRECT_PAT
-    // DIRECTLY inject simple pattern gen from FALL-2013-CP1
+
+end else if (COLT45_TESTPAT == 3) begin:DIRECT_PAT
+// *** DIRECTLY inject simple pattern gen from FALL-2013-CP1 ***
     PatternGenerator #(
         .CLOCK_HZ(50_000_000), //DVI Clock
         .SCREEN_WIDTH(800), .SCREEN_HEIGHT(600),
@@ -185,18 +172,16 @@ endmodule
 
 /* RAMBLINGS:
 
-    // DDR "FIFOs" are simply muxed to a shared FIFO that we don't get to stall, without
-    //   any individual buffering.  We are forced to grab (or lose) anything coming back
-    //   on rdf_out, hence the direct tie of rdf_rd_en & pixel_fifo.wr_en to rdf_valid
-    //   and futility of using pixel_fifo.full checking.  We face an increment of 256-bits
-    //   for each 1 request on af_addr_din accepted...turning into 2 responses (128-bits each)
-    //   eventually out of rdf_dout...turning into 8 reads (32-bits each) out of DVI clocked
-    //   side of pixel_fifo.  Pre-built pixel_fifo is 2048 deep on the write-side, MIG related
-    //   DDR FIFOs appear to be 1024 deep (af & rdf each).  Worst case latency through entire
-    //   chain is pretty horrible (and in theory the PixelFeeder can get starved) but regular
-    //   use should be better?  We have advantage of 1-to-8 ratio DDR requests to DVI reads at
-    //   either identical or comparable clock rates.  Seems obvious trick is to rebuild the
-    //   pixel_fifo to include either counter or range assertion support...but I guess we are
-    //   meant to confront issue ourselves to grasp it best.
-
+    DDR "FIFOs" are simply muxed to shared FIFOs that we don't get to stall, without
+    any individual buffering.  We are forced to grab (or lose) anything coming back
+    on rdf_out, hence the direct tie of rdf_rd_en & pixel_fifo.wr_en to rdf_valid
+    and futility of using pixel_fifo.full checking.  We face an increment of 256-bits
+    for each 1 request on af_addr_din accepted...turning into 2 responses (128-bits each)
+    eventually out of rdf_dout...turning into 8 reads (32-bits each) out of DVI clocked
+    side of pixel_fifo.  We have advantage of 1-to-8 ratio DDR requests to DVI reads at
+    either identical or comparable clock rates.  One obvious trick is to rebuild the
+    pixel_fifo to include either counter or range assertion support...but I guess we are
+    meant to confront issue ourselves to grasp it best.  I'm using a simple signal/ack
+    whereby the DVI side informs the CPU-clocked side of chunks of read data...allowing
+    a coarse-grain count to be kept at the request initiating spot.
 */
