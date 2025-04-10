9796d58510e1559724d5ea74b7b6844724b12194
Erik Rogers <1408777+cdr4eelz@users.noreply.github.com>
2025-04-09 17:59:04 -0700

    First functional simulation with xsim and in non-project mode. Also, resurrected video pattern generator code PatternGenerator.v

---
+++ hardware/testbench/make_sim.mk
@@ -0,0 +1,18 @@
+NAME = ALU_tb
+
+
+vlog xsim.dir : $(NAME).prj $(NAME).v
+	xvlog --prj $(NAME).prj --include ..
+
+elab xsim.dir/xil_defaultlib.$(NAME) : xsim.dir
+	xelab --debug typical xil_defaultlib.$(NAME)
+
+sim xil_defaultlib.$(NAME).wdb : xsim.dir/xil_defaultlib.$(NAME)
+	xsim --gui --view $(NAME).wcfg  xil_defaultlib.$(NAME)
+
+
+clean :
+	rm -rf .Xil xsim.dir *.log *.pb *.jou
+
+.phony: vlog elab sim clean
+
