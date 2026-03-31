e733b71d73f56f81caec1a48b3f8e674469ca122
Erik Rogers <1408777+cdr4eelz@users.noreply.github.com>
2026-03-31 13:36:21 -0700

    Adjusted software makefiles to use "mips-unknown-elf" rather than "mips-linux-gnu" as tool prefix (gcc, ld, as, etc).

--- software/echoplus_i/echoplus.c
+++ software/echoplus_i/echoplus.c
@@ -43,7 +43,7 @@ void TRAN_cstr(const char *str)
 
 
 const char greeting[] = "\r\n\r\n### ECHOPLUS ###\r\n\r\n";
-int max_loops = 2;
+const int max_loops = 2;
 
 int main(void)
 {
@@ -58,6 +58,6 @@ int main(void)
 		}
 		if (++offset > max_loops) break;
 	}
-	max_loops++;
+	//max_loops++;
 	return 0;
 }
